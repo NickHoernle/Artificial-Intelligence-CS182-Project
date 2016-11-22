62282ebdffcb6357543cb8575ce71faccc1a841f
Sophie Hilgard <ashilgard@gmail.com>
2016-11-21 22:28:37 -0500

    added connection class

--- intersections_and_roads.py
+++ intersections_and_roads.py
@@ -2,19 +2,36 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 
+class connection:
+    def __init__(self, id, source, target, distance):
+        self.id = id
+        self.source = source
+        self.target = target
+        self.distance = distance
+        
+    def add_accidents(self, accidents):
+        self.accidents = accidents
+        
+    def get_child(self, node_id):
+        if node_id==self.source:
+            return self.target
+        else:
+            return self.source
+        
+
 class node:
     def __init__(self, id, x, y):
         self.id = id
         self.x = x
         self.y = y
-        self.connections = dict()
+        self.connections = set()
     
     def id(self):
         return self.id
-    
-    def add_connection(self, connection_id, distance):
+    #make connection_ids the road centerline IDs
+    def add_connection(self, connection_id):
         if connection_id not in self.connections:
-            self.connections[connection_id] = distance
+            self.connections.add(connection_id)
     
     def get_connections(self):
         return self.connections
@@ -29,7 +46,7 @@ class node:
 def euclidean_distance(p1, p2):
     return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
-def follow_road(intersection, intersections, street_centerline, intersection_graph):
+def follow_road(intersection, intersections, street_centerline, intersection_graph, connection_dict):
     if intersection.id not in intersection_graph:
         intersection_graph[intersection.id] = node(intersection.id, intersection.P_X, intersection.P_Y)
     this_node = intersection_graph[intersection.id]
@@ -49,10 +66,13 @@ def follow_road(intersection, intersections, street_centerline, intersection_gra
             distance = euclidean_distance(new_node.get_x_y(), this_node.get_x_y())
 
             if distance < 10000: # I don't understand why.... Suggestions welcome
-                this_node.add_connection(new_node.id, distance)
-                new_node.add_connection(this_node.id, distance)
+                this_node.add_connection(street.Street_ID)
+                new_node.add_connection(street.Street_ID)
+                if street.Street_ID not in connection_dict:
+                    connection_dict[street.Street_ID] = connection(street.Street_ID, this_node.id, new_node.id, distance)
 
 def build_intersection_graph(intersections, street_centerline):
     intersection_graph = dict()
-    intersections.apply(follow_road, axis=1, args=[intersections, street_centerline, intersection_graph])
+    connection_dict = dict()
+    intersections.apply(follow_road, axis=1, args=[intersections, street_centerline, intersection_graph, connection_dict])
     return intersection_graph
