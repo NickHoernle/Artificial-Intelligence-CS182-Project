9ace5f44db6ee46588df4dd6a203813322d72609
Nikhila Ravi <nikkiravi92@gmail.com>
2016-12-04 01:40:40 -0500

    Updates plotting of starting points

--- search.py
+++ search.py
@@ -72,8 +72,9 @@ def local_beam_search(k, intersection_graph, connection_dict, cost_function, heu
     # select k inital random starting points
     k_points = np.random.choice(candidate_nodes, k, replace=False)
     # calculate the costs for the points
-    costs = [np.sum([cost(start_node, k_point, intersection_graph, connection_dict, cost_function, heuristic) for start_node in starting_points]) for k_point in k_points]
-
+    # costs = [np.sum([cost(start_node, k_point, intersection_graph, connection_dict, cost_function, heuristic) for start_node in starting_points]) for k_point in k_points]
+    all_costs = [[cost(start_node, k_point, intersection_graph, connection_dict, cost_function, heuristic) for start_node in starting_points] for k_point in k_points]
+    costs = [(max(costs) - min(costs)) for costs in all_costs]
     # save the min cost as the current best
     best_cost = min(costs)
     best_centroid = k_points[np.argmin(costs)]
@@ -96,8 +97,8 @@ def local_beam_search(k, intersection_graph, connection_dict, cost_function, heu
         successor_nodes = np.array([get_node((node, connection)) for (node, connection) in successor_connections])
 
         # evaluate costs of all successors
-        successor_costs = np.array([np.sum([cost(start_node, successor_node, intersection_graph, connection_dict, cost_function, heuristic) for start_node in starting_points]) for successor_node in successor_nodes])
-
+        all_successor_costs = np.array([[cost(start_node, successor_node, intersection_graph, connection_dict, cost_function, heuristic) for start_node in starting_points] for successor_node in successor_nodes])
+        successor_costs = np.array([(max(costs) - min(costs)) for costs in all_successor_costs])
         # retain best k successors
         best_k_indices = np.argsort(successor_costs, axis=0)[:k]
         best_k_costs = successor_costs[best_k_indices]
